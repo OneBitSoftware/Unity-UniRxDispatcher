de9596c7bb32364ac80a039d012a82286dabf2ba
Radi A <radi.a@live.com.au>
2017-06-22 11:46:44 +0300

    Bulk commit of all files

---
+++ Assets/Scripts/FrameCounter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameCounter : MonoBehaviour {
+    public Text TextFieldToUpdate;
+	
+	void Update () {
+
+        if (TextFieldToUpdate != null)
+            TextFieldToUpdate.text = Time.frameCount.ToString();	
+	}
+}
---
+++ Assets/Scripts/PerformWork.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PerformWork : MonoBehaviour
+{
+    public Text TextFieldToUpdate;
+
+
+    public void DoSingleItemWork_Click()
+    {
+        RxDispatcher.Instance.Enqueue(() =>
+        {
+            LongOperation("3000ms");
+        });
+    }
+
+    public void DoMultipleItemsWork_Click()
+    {
+        RxDispatcher.Instance.Enqueue(() => 
+            LongOperation("3000ms")
+         );
+
+        RxDispatcher.Instance.Enqueue(() => 
+            LongOperation("6000ms")
+        );
+
+        RxDispatcher.Instance.Enqueue(() => 
+            LongOperation("9000ms")
+        );
+    }
+
+    public void DoBulkWork_Click()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            RxDispatcher.Instance.Enqueue(() =>
+                {
+                    //TextFieldToUpdate.text = "Running item " + i.ToString();
+                }
+            );
+        }
+    }
+
+    private void LongOperation(string delayText)
+    {
+        System.Threading.Thread.Sleep(3000); // main thread blocker
+        // Update UI - proves we work on the main thread
+        TextFieldToUpdate.text = delayText + " blocking work complete. " + System.DateTime.Now.ToString();
+    }
+}
